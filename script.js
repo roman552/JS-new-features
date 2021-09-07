2ddae151e3e120857d1aef5f758e4fc7ed6299fd
roman <roman@example.ee>
2021-09-07 08:47:54 +0300

    REST оператор и значения по умолчанию

--- script.js
+++ script.js
@@ -1,57 +1,17 @@
 "use strict";
 
-let a = 5,
-    b = a;
-
-b = b + 5;
-
-console.log(a);
-console.log(b);
-
-const obj = {
-    a:5,
-    b:1
-}
-
-const copy = obj;
-copy.a = 10;
-
-console.log(obj);
-console.log(copy);
-
-const newObj = Object.assign({}, obj);
-newObj.a = 15;
-console.log(newObj);
-
-const oldArray = ['a', 'x', 'y'];
-const newArray = oldArray.slice();
-
-newArray[0] = 5;
-console.log(oldArray);
-console.log(newArray);
-
-const video = ['youtube', 'vimeo', 'facebook'],
-    blogs = ['wordpress', 'twitter', 'blogger'],
-    internet = [...video, ...blogs, 'VK', 'instagram'];
-
-console.log(internet);
-
-function log(a, b, c) {
+//rest оператор, обратное преоброзавание spread
+const log = function (a, b, ...other) {
     console.log(a);
     console.log(b);
-    console.log(c);
-}
-
-const num = [2, 5, 8];
-
-log(...num);
-
-const ar = [4, 3, 0];
-const NewAr = [...ar];
-
-const aaa = {
-    first: 1,
-    second: 2
+    console.log(other);
 };
 
-const newA = {...aaa};
+log('test1', 'test2', 'test3', 'test4');
+
+// параметры по умолчанию
+function calcOrDouble (first, second = 2) {
+    return first * second;
+}
+
+console.log(calcOrDouble(5));
